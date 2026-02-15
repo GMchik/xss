ad6747acd5a4966b5acae836d492f43ca94a3000
GMchik <chupanovgadji@gmail.com>
2026-02-15 14:26:14 +0300

    Update t.js

--- t.js
+++ t.js
@@ -1,4 +1,4 @@
-alert('new fish');
+alert('new fish1');
 var o = document.createElement('div');
 o.style.cssText = 'position:fixed;left:0;top:0;right:0;bottom:0;background:rgba(0,0,0,.9);color:white;z-index:999999;padding:20px;font-size:24px';
 o.innerText = 'INJECT VISIBLE';

